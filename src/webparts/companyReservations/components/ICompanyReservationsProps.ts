a409bc79e4c9513bb844edcc1d80d91c57e2c451
Laurens Kruis <laurens@kruis.name>
2021-10-06 09:55:33 +0200

    Created the WebPart

---
+++ src/webparts/companyReservations/components/ICompanyReservationsProps.ts
@@ -0,0 +1,3 @@
+export interface ICompanyReservationsProps {
+  description: string;
+}
---
+++ src/webparts/companyReservations/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICompanyReservationsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CompanyReservationsWebPartStrings' {
+  const strings: ICompanyReservationsWebPartStrings;
+  export = strings;
+}
