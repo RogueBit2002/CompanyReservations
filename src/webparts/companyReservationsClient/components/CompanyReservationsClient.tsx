00b55244b6211c7e078cc41c57b427e00fc39b5c
Laurens Kruis <laurens@kruis.name>
2021-10-07 15:49:51 +0200

    Implemented the Pivot element, and played around with css

--- src/webparts/companyReservationsClient/components/CompanyReservationsClient.tsx
+++ src/webparts/companyReservationsClient/components/CompanyReservationsClient.tsx
@@ -3,6 +3,7 @@ import styles from './CompanyReservationsClient.module.scss';
 import { ICompanyReservationsClientProps } from './ICompanyReservationsClientProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { ICompanyReservationsClientState } from './ICompanyReservationsClientState';
+import { Pivot, PivotItem } from 'office-ui-fabric-react';
 
 export default class CompanyReservationsClient extends React.Component<ICompanyReservationsClientProps, ICompanyReservationsClientState> {
 
@@ -20,12 +21,14 @@ export default class CompanyReservationsClient extends React.Component<ICompanyR
 				<div className={ styles.container }>
 					<div className={ styles.row }>
 						<div className={ styles.column }>
-							<span className={ styles.title }>Welcome to SharePoint!</span>
-							<p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-							<p className={ styles.description }>{escape(this.props.description)}</p>
-							<a href="https://aka.ms/spfx" className={ styles.button }>
-							<span className={ styles.label }>Learn more</span>
-							</a>
+						<Pivot className={styles.pivot}>
+							<PivotItem headerText="Tab 1">
+								<div className={styles.pivotContent}>{"Pivot 1 content"}</div>
+							</PivotItem>
+							<PivotItem headerText="Tab 2">
+								<div className={styles.pivotContent}>{"Pivot 2 content"}</div>
+							</PivotItem>
+						</Pivot>
 						</div>
 					</div>
 				</div>
