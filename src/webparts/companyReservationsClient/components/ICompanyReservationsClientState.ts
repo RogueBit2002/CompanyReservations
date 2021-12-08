867f17b9bae93fb5a83cab723706373f9027a4cd
Laurens Kruis <laurens@kruis.name>
2021-12-08 13:43:33 +0100

    Removed unused imports

--- src/webparts/companyReservationsClient/components/ICompanyReservationsClientState.ts
+++ src/webparts/companyReservationsClient/components/ICompanyReservationsClientState.ts
@@ -1,9 +1,4 @@
-import { IViewField } from "@pnp/spfx-controls-react/lib/ListView";
-import { Reservation } from "../../../companyReservations/Reservation";
-import { Workspace } from "../../../companyReservations/Workspace";
-import { List } from "../../../services/Sharepoint/List";
-import { User } from "../../../services/Sharepoint/User";
-
 export interface ICompanyReservationsClientState
 {
+
 }
